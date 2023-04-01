91b5af3e320979ffc15684120cd118b683e045e8
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-04-01 20:34:23 +0600

    footer Blogs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 };
